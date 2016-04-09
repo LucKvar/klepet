9607a6e3c78b932e743899d98cb9b2cdd53cb645
Lucijan Kvar <lk5628@student.uni-lj.si>
2016-04-09 23:14:44 +0000

    Pošlji zasebno

--- public/js/klepet_UI.js
+++ public/js/klepet_UI.js
@@ -99,6 +99,12 @@ $(document).ready(function() {
     for (var i=0; i < uporabniki.length; i++) {
       $('#seznam-uporabnikov').append(divElementEnostavniTekst(uporabniki[i]));
     }
+    
+    $('#seznam-uporabnikov div').click(function() {
+        var imeUporabnika = $(this).text();
+        $('#poslji-sporocilo').val('/zasebno \"' + imeUporabnika +'\"');
+        $('#poslji-sporocilo').focus();
+    })
   });
 
   setInterval(function() {
